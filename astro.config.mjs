6dfbf1943ba5312b8efe7d3cbf9879c0ba2da56b
Lukas Prokopec <koki.prokopec@gmail.com>
2025-11-21 11:39:43 +0100

    fix: stylesheet loading

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-    site: 'https://l-prokopec.github.io/bartys-tattoo',
-    base: '/bartys-tattoo/',
+  site: 'https://l-prokopec.github.io',
+  base: 'bartys-tattoo',
 });
