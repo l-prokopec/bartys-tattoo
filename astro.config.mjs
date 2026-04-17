0df0169288e612921986553ac2a2e79d0f464b60
Lukas Prokopec <koki.prokope@gmail.com>
2026-04-17 20:40:43 +0200

    custom domain fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
-    site: 'https://l-prokopec.github.io',
-    base: '/bartys-tattoo/',
+  site: "https://www.bartystattoo.cz",
 });
